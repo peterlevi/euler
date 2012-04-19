a77bde501f72d9566a4e23a19482f904620d50b2
Peter Levi <peterlevi@peterlevi.com>
2012-04-19 17:09:51 +0300

    46, haskell

---
+++ 046.hs
@@ -0,0 +1,20 @@
+module Main where
+
+primes = [x | x <- [2..], isPrime x]
+
+isPrime :: Integer -> Bool
+isPrime x = null $ filter (\y ->  x `mod` y == 0) $ takeWhile (\y ->  y*y <= x) [2..]
+
+bad :: Integer -> Bool
+bad x
+  | isPrime x = False
+  | otherwise = null [p | p <- takeWhile (<x) primes, is2sq (x - p)]
+
+isInt :: Double -> Bool
+isInt x = x == fromInteger (round x)
+
+is2sq :: Integer -> Bool
+is2sq x = isInt $ sqrt $ (fromIntegral x) / 2
+
+main = print $ head $ filter bad [3,5..]
+
