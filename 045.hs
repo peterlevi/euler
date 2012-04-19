2e1bff60a21d5f74ea615b1941723b655979c651
Peter Levi <peterlevi@peterlevi.com>
2012-04-19 11:06:06 +0300

    removed 045.hs

--- 045.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import qualified Data.Set as Set
-
-main = print $ Set.intersection (Set.intersection a b) c
-            where a = foldl (flip Set.insert) Set.empty [n*(n+1)/2 | n <- [1..10000]]
-                  b = foldl (flip Set.insert) Set.empty [n*(3*n-1)/2 | n <- [1..10000]] 
-                  c = foldl (flip Set.insert) Set.empty [n*(2*n-1) | n <- [1..10000]] 
